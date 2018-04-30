a625bd44a368687492f0df9bbf2c311348199c45
kromero <kromero88@gmail.com>
2018-04-29 22:00:45 -0400

    not sure

--- trackster/resources/js/trackster.js
+++ trackster/resources/js/trackster.js
@@ -1,10 +1,20 @@
 var Trackster = {};
 
+/* Search Icon Click Event*/
+
 $(document).ready(function() {
   $('#search-btn').click(function() {
     console.log('it works');
   });
 });
+
+/* Head Click Event Test */
+$(document).ready(function(){
+  $('#head').click(function() {
+    console.log('icannot fucking believe this shit you mother fuckers');
+  });
+});
+
 /*
   Given an array of track data, create the HTML for a Bootstrap row for each.
   Append each "row" to the container in the body to display all tracks.

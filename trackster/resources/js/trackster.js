f05fc56677251aa7463f0eb3d814891ff9b2afdb
kromero <kromero88@gmail.com>
2018-04-30 21:08:35 -0400

    finished trackster

--- trackster/resources/js/trackster.js
+++ trackster/resources/js/trackster.js
@@ -14,12 +14,28 @@ $(document).ready(function() {
   Append each "row" to the container in the body to display all tracks.
 */
 Trackster.renderTracks = function(tracks) {
+  var $trackList = $('#track-list');
 
+  $trackList.empty();
 
-// FILL THIS METHOD NEXT!!!!
+  for (var trackIndex = 0; trackIndex < tracks.length; trackIndex++) {
+    var track = tracks[trackIndex];
+    var mediumAlbumArt = track.image[1]["#text"];
+    var htmlTrackRow = '<div class="row track">' +
+     '  <div class="col-xs-1 col-xs-offset-1 play-button">' +
+     '    <a href="'+ track.url + '" target="_blank">' +
+     '      <i class="fa fa-play-circle-o fa-2x"></i>' +
+     '    </a>' +
+     '  </div>' +
+     '  <div class="col-xs-4">' + track.name + '</div>' +
+     '  <div class="col-xs-2">' + track.artist + '</div>' +
+     '  <div class="col-xs-2"><img src="' + mediumAlbumArt + '"/></div>' +
+     '  <div class="col-xs-2">' + track.listeners + '</div>' +
+     '</div>';
 
-
-};
+     $trackList.append(htmlTrackRow);
+    }
+  };
 
 /*
   Given a search term as a string, query the LastFM API.
@@ -29,7 +45,8 @@ Trackster.searchTracksByTitle = function(title) {
   $.ajax({
     url: 'http://ws.audioscrobbler.com/2.0/?method=track.search&track=' + title + '&api_key='+API_KEY+'&format=json',
     success: function(response) {
-      console.log(response);
+      Trackster.renderTracks(response.results.trackmatches.track);
+      console.log('fuck you im learning and javascript is not at all intuitive')
     }
   });
 };

cccff3f55e1a8eb67d0d9fd7f4effaa5f429a284
kromero <kromero88@gmail.com>
2019-06-14 22:28:07 -0400

    installed scrollmagic on this test project
    was able to create my first scroll animation 'fade-in'
    
    pushing to web to see it work on different browsers and see if it works on mobile.

--- parrallax/resources/js/main.js
+++ parrallax/resources/js/main.js
@@ -1,6 +1,38 @@
-var controller = new ScrollMagic.Controller();
- var scene = new ScrollMagic.Scene({
-   triggerElement: '.red-cube'
- })
- .setClassToggle('.red-cube', 'show')
- .addTo(controller);
+console.log('up and running');
+
+$(document).ready(function(){
+  //Init ScrollMagic
+  var controller = new ScrollMagic.Controller();
+  // build a scene
+  var ourScene = new ScrollMagic.Scene({
+    triggerElement: '#project01'
+  })
+  .setClassToggle('#project01', 'fade-in') //add class to project 01
+  // .addIndicators({
+  //   name: 'fade scene',
+  //   colorTrigger: 'black',
+  //   indent: 200,
+  //   colorStart: '#75C695'
+  // }) //adds indicators for debugging
+  .addTo(controller);
+
+  var controller2 = new ScrollMagic.Controller();
+  // build a scene
+  var ourScene2 = new ScrollMagic.Scene({
+    triggerElement: '#project02'
+  })
+  .setClassToggle('#project02', 'fade-in') //add class to project 01
+  // .addIndicators()
+  .addTo(controller2);
+
+  var controller3 = new ScrollMagic.Controller();
+  // build a scene
+  var ourScene3 = new ScrollMagic.Scene({
+    triggerElement: '#project03'
+  })
+  .setClassToggle('#project03', 'fade-in') //add class to project 01
+  // .addIndicators()
+  .addTo(controller3);
+
+
+});
